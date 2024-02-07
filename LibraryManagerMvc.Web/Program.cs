4bdc0c1f711e376e90dcb2420b4b104d8bc75f19
Philip Newman <philipeano@gmail.com>
2024-02-07 11:56:23 +0100

    Register dbcontext for dependency injection

--- LibraryManagerMvc.Web/Program.cs
+++ LibraryManagerMvc.Web/Program.cs
@@ -1,7 +1,22 @@
+using LibraryManagerMvc.Data;
+using LibraryManagerMvc.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("LibraryManagerMvcConnStr");
+builder.Services.AddDbContext<LibraryManagerMvcContext>(options => options.UseSqlServer(connectionString));
+
+/* Use any of the 3 service lifetimes to register your user-define services
+ *     AddSingleton - Creates one instance of the service and uses it throughout the lifetime of the app, for all requests
+ *     AddScoped - Creates a new instance of the service and uses it throughout a particular request/response cycle
+ *     AddTransient - Creates a new instance each time the service is requested, whether within the same request or not.
+ */
+
+builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 var app = builder.Build();
 
