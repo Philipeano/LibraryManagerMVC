0d8b341440478d9045a52abe04adfebfc87382b1
Philip Newman <philipeano@gmail.com>
2024-02-16 08:59:37 +0100

    Implement actions and views for CRUD tasks

--- LibraryManagerMvc.Web/Controllers/BooksController.cs
+++ LibraryManagerMvc.Web/Controllers/BooksController.cs
@@ -1,15 +1,16 @@
 ﻿using LibraryManagerMvc.Data.Entities;
 using LibraryManagerMvc.Data.Enums;
 using LibraryManagerMvc.Data.Repositories;
+using LibraryManagerMvc.Web.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NuGet.Versioning;
 
 namespace LibraryManagerMvc.Web.Controllers
 {
+    // [Route("books/")]
     public class BooksController : Controller
     {
-
         private readonly IBookRepository _bookRepository;
 
         public BooksController(IBookRepository bookRepository)
@@ -23,13 +24,46 @@ namespace LibraryManagerMvc.Web.Controllers
             return View();
         }
 
-        // GET: BooksController/Details/5
-        public ActionResult Details(int id)
+        // GET: BooksController/List
+        [Route("books/view-all")]
+        public ActionResult List(string? searchTerm)
         {
-            return View();
+            var booklistViewModel = new BookListViewModel();
+
+            var booksToDisplay = _bookRepository.GetAllBooks();
+
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                booklistViewModel.Books = booksToDisplay;
+                booklistViewModel.SearchTerm = string.Empty;
+                return View("ViewAll", booklistViewModel);
+            }
+            else
+            {
+                var filteredBooks = booksToDisplay.Where(b => b.Title.Contains(searchTerm, StringComparison.CurrentCultureIgnoreCase)
+                                                           || b.Author.Contains(searchTerm, StringComparison.CurrentCultureIgnoreCase));
+                booklistViewModel.Books = filteredBooks;
+                booklistViewModel.SearchTerm = searchTerm;
+                return View("ViewAll", booklistViewModel);
+            }
+        }
+
+        // GET: BooksController/Details/some-guid-value
+        [Route("books/details/{id}")]
+        public ActionResult Details(Guid id)
+        {
+            var bookToDisplay = _bookRepository.GetBook(id);
+
+            if (bookToDisplay == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookToDisplay);
         }
 
         // GET: BooksController/Create
+        [Route("books/create")]
         public ActionResult Create()
         {
             var newBook = new Book(); // In future, define a ViewModel for this purpose
@@ -39,7 +73,8 @@ namespace LibraryManagerMvc.Web.Controllers
         // POST: BooksController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        [Route("books/create")]
+        public ActionResult Create(Book postedBook)
         {
             try
             {
@@ -50,11 +85,11 @@ namespace LibraryManagerMvc.Web.Controllers
 
                 var newBook = new Book()
                 {
-                    Id = new Guid(),
-                    Title = collection["Title"],
-                    Author = collection["Author"],
-                    Category = Enum.Parse<Category>(collection["Category"]),
-                    YearPublished = int.Parse(collection["YearPublished"]),
+                    Id = Guid.NewGuid(),
+                    Title = postedBook.Title,
+                    Author = postedBook.Author,
+                    Category = postedBook.Category,
+                    YearPublished = postedBook.YearPublished,
                     Created = DateTime.Now,
                     Updated = DateTime.Now
                 };
@@ -68,19 +103,36 @@ namespace LibraryManagerMvc.Web.Controllers
             }
         }
 
-        // GET: BooksController/Edit/5
-        public ActionResult Edit(int id)
+        // GET: BooksController/Edit/some-guid-value
+        [Route("books/edit/{id}")]
+        public ActionResult Edit(Guid id)
         {
-            return View();
+            var bookToUpdate = _bookRepository.GetBook(id);
+            if (bookToUpdate == null)
+            {
+                return NotFound();
+            }
+            return View(bookToUpdate);
         }
 
-        // POST: BooksController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        [HttpPost, Route("books/edit/{id}"), ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid id, Book postedBook)
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return View();
+                }
+
+                if (id != postedBook.Id)
+                {
+                    return BadRequest();
+                }
+
+                postedBook.Updated = DateTime.Now;
+                _bookRepository.UpdateBook(postedBook);
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -89,20 +141,34 @@ namespace LibraryManagerMvc.Web.Controllers
             }
         }
 
-        // GET: BooksController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: BooksController/Delete/some-guid-value
+        [Route("books/delete/{id}")]
+        public ActionResult Delete(Guid id)
         {
-            return View();
+            var bookToDelete = _bookRepository.GetBook(id);
+            if (bookToDelete == null)
+            {
+                return NotFound();
+            }
+            return View(bookToDelete);
         }
 
-        // POST: BooksController/Delete/5
+        // POST: BooksController/Delete/some-guid-value
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        [Route("books/delete/{id}")]
+        public ActionResult ConfirmDelete(Guid id)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var bookToDelete = _bookRepository.GetBook(id);
+                if (bookToDelete == null)
+                {
+                    return NotFound();
+                }
+
+                _bookRepository.DeleteBook(id);
+                return RedirectToAction(nameof(List));
             }
             catch
             {
---
+++ LibraryManagerMvc.Web/Models/BookListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagerMvc.Data.Entities;
+using System.ComponentModel;
+
+namespace LibraryManagerMvc.Web.Models
+{
+	public class BookListViewModel
+	{
+		[DisplayName("Search")]
+		public string SearchTerm { get; set; }
+
+		public IEnumerable<Book> Books { get; set; }
+	}
+}
